20f8885989e3cec26e0d5ef3e6169534796eb816
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-01-16 17:04:47 +0600

    add categories initially

--- Deliveroo2/screens/HomeScreen.js
+++ Deliveroo2/screens/HomeScreen.js
@@ -9,6 +9,8 @@ import {
   AdjustmentsVerticalIcon,
 } from 'react-native-heroicons/outline'
 import { TextInput } from 'react-native-gesture-handler'
+import { ScrollView } from 'react-native-gesture-handler'
+import Categories from '../components/Categories'
 
 const HomeScreen = () => {
   const navigation = useNavigation()
@@ -22,7 +24,7 @@ const HomeScreen = () => {
   return (
     <SafeAreaView className='bg-white pt-5'>
       {/* Header */}
-      <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+      <View className='flex-row pb-3 items-center mx-4 space-x-2 px-4'>
         <Image
           source={{
             uri: 'https://links.papareact.com/wru',
@@ -38,9 +40,10 @@ const HomeScreen = () => {
         </View>
         <UserIcon size={35} color='#00CCBB' />
       </View>
+
       {/* Search */}
-      <View className='flex-row items-center space-x-2 pb-2 mx-4'>
-        <View className='flex-row space-x-2 bg-gray-200 p-3'>
+      <View className='flex-row items-center space-x-2 pb-2 mx-4 px-4'>
+        <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3'>
           <MagnifyingGlassIcon color='gray' size={20} />
           <TextInput
             placeholder='Restaurants and Cuisines'
@@ -49,6 +52,13 @@ const HomeScreen = () => {
         </View>
         <AdjustmentsVerticalIcon color='#00CCBB' />
       </View>
+
+      {/* Body */}
+      <ScrollView className='bg-gray-100'>
+        {/* Categories */}
+        <Categories />
+        {/* Featured Rows */}
+      </ScrollView>
     </SafeAreaView>
   )
 }
