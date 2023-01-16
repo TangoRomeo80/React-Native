09751f6bf627389eb1997c2346adc768b3c836d2
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-01-16 13:27:04 +0600

    add react-navigation

--- Deliveroo2/App.js
+++ Deliveroo2/App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center'>
-      <Text className="text-slate-800">Open up App.js to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <NavigationContainer>
+      <View className='flex-1 items-center justify-center'>
+        <Text className='text-red-500'>
+          Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style='auto' />
+      </View>
+    </NavigationContainer>
   )
 }
