d7a194adb6c7915e62603cc42939fe6afd252834
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-01-16 16:16:17 +0600

    implemented search

---
+++ Deliveroo2/screens/HomeScreen.js
@@ -0,0 +1,56 @@
+import { View, Text, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {
+  ChevronDownIcon,
+  UserIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from 'react-native-heroicons/outline'
+import { TextInput } from 'react-native-gesture-handler'
+
+const HomeScreen = () => {
+  const navigation = useNavigation()
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, [navigation])
+
+  return (
+    <SafeAreaView className='bg-white pt-5'>
+      {/* Header */}
+      <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+        <Image
+          source={{
+            uri: 'https://links.papareact.com/wru',
+          }}
+          className='h-7 w-7 bg-gray-300 rounded-full'
+        />
+        <View className='flex-1'>
+          <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+          <Text className='font-bold text-xl'>
+            Current Location
+            <ChevronDownIcon size={20} color='#00CCBB' />
+          </Text>
+        </View>
+        <UserIcon size={35} color='#00CCBB' />
+      </View>
+      {/* Search */}
+      <View className='flex-row items-center space-x-2 pb-2 -mx-44'>
+        <View className='flex-row space-x-2 bg-gray-200 p-3'>
+          <MagnifyingGlassIcon color='gray' size={20} />
+          <TextInput
+            placeholder='Restauurants and Cuisines'
+            keyboardType='default'
+          />
+        </View>
+        <AdjustmentsVerticalIcon color='#00CCBB' />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
