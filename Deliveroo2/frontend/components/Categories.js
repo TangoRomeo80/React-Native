6f1672b13e2b8f7e2f0e630f59390b7fdb6c6e31
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-01-20 23:05:41 +0600

    develop basic homescreen for deliveroo

--- Deliveroo2/frontend/components/Categories.js
+++ Deliveroo2/frontend/components/Categories.js
@@ -1,8 +1,17 @@
 import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CategoryCard from './CategoryCard'
+import sanityClient, { urlFor } from '../sanity'
 
 const Categories = () => {
+  const [categorises, setCategories] = useState([])
+
+  useEffect(() => {
+    sanityClient.fetch(`*[_type == "category"]`).then((data) => {
+      setCategories(data)
+    })
+  }, [])
+
   return (
     <ScrollView
       contentContainerStyle={{
@@ -13,12 +22,13 @@ const Categories = () => {
       showsHorizontalScrollIndicator={false}
     >
       {/* CategoryCards */}
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='test 1' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='test 2' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='test 3' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='test 4' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='test 5' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='test 6' />
+      {categorises.map((category) => (
+        <CategoryCard
+          key={category._id}
+          imgUrl={urlFor(category.image).url()}
+          title={category.name}
+        />
+      ))}
     </ScrollView>
   )
 }
--- Deliveroo2/frontend/screens/HomeScreen.js
+++ Deliveroo2/frontend/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, Image, ScrollView, TextInput } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect, useState, useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {
@@ -10,15 +10,33 @@ import {
 } from 'react-native-heroicons/outline'
 import Categories from '../components/Categories'
 import FeaturedRow from '../components/FeaturedRow'
+import sanityClient from '../sanity'
 
 const HomeScreen = () => {
   const navigation = useNavigation()
+  const [featuredCategories, setFeaturedCategories] = useState([])
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     })
   }, [navigation])
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "featured"] {
+      ...,
+      restaurants[]->{
+        ...,
+        dishes[]->
+      }
+    }`
+      )
+      .then((data) => {
+        setFeaturedCategories(data)
+      })
+  }, [])
 
   return (
     <SafeAreaView className='bg-white pt-5'>
@@ -53,30 +71,25 @@ const HomeScreen = () => {
       </View>
 
       {/* Body */}
-      <ScrollView className='bg-gray-100 overflow-auto'
+      <ScrollView
+        className='bg-gray-100 overflow-auto'
         style={{ marginBottom: 110 }}
       >
         {/* Categories */}
         <Categories />
 
         {/* Featured*/}
-        <FeaturedRow
-          id='123'
-          title='Featured'
-          description='Paid placements from our partners'
-        />
-        {/* Tasty Discounts*/}
-        <FeaturedRow
-          id='456'
-          title='Tasty Discounts'
-          description='Everyone has been enjoying these deals'
-        />
-        {/* Offers near you*/}
-        <FeaturedRow
-          id='789'
-          title='Offers near you'
-          description='Why not support your local restaurants'
-        />
+
+        {featuredCategories?.map((category) => (
+          <FeaturedRow
+            key={category._id}
+            id={category._id}
+            title={category.name}
+            description={category.short_description}
+          />
+        ))}
+
+        
       </ScrollView>
     </SafeAreaView>
   )
