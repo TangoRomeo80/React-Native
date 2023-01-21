5dd52fe852164754b870abacdfc42ff1510144f2
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-01-21 14:56:48 +0600

    implemented till redux

--- Deliveroo2/frontend/screens/RestaurantScreen.js
+++ Deliveroo2/frontend/screens/RestaurantScreen.js
@@ -10,6 +10,7 @@ import {
   QuestionMarkCircleIcon,
   StarIcon,
 } from 'react-native-heroicons/solid'
+import DishRow from '../components/DishRow'
 
 const RestaurantScreen = () => {
   const navigation = useNavigation()
@@ -35,7 +36,7 @@ const RestaurantScreen = () => {
     },
   } = useRoute()
   return (
-    <SafeAreaView className='bg-white'>
+    <SafeAreaView>
       <ScrollView>
         <View className='relative'>
           <Image
@@ -77,6 +78,20 @@ const RestaurantScreen = () => {
             </Text>
             <ChevronRightIcon color='#00CCBB' />
           </TouchableOpacity>
+        </View>
+        <View>
+          <Text className='px-4 pt-6 mb-3 font-bold text-xl'>Menu</Text>
+          {/* Dishrows */}
+          {dishes.map((dish) => (
+            <DishRow
+              key={dish._id}
+              id={dish._id}
+              name={dish.name}
+              description={dish.short_description}
+              price={dish.price}
+              image={dish.image}
+            />
+          ))}
         </View>
       </ScrollView>
     </SafeAreaView>
