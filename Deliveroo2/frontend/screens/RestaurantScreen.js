a1153a147bb3cecd44d6a67caaab7cde2b10efb2
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-01-22 18:56:26 +0600

    implement view basket button

--- Deliveroo2/frontend/screens/RestaurantScreen.js
+++ Deliveroo2/frontend/screens/RestaurantScreen.js
@@ -11,6 +11,7 @@ import {
   StarIcon,
 } from 'react-native-heroicons/solid'
 import DishRow from '../components/DishRow'
+import BasketIcon from '../components/BasketIcon'
 
 const RestaurantScreen = () => {
   const navigation = useNavigation()
@@ -37,6 +38,7 @@ const RestaurantScreen = () => {
   } = useRoute()
   return (
     <SafeAreaView>
+      <BasketIcon />
       <ScrollView>
         <View className='relative'>
           <Image
@@ -79,7 +81,7 @@ const RestaurantScreen = () => {
             <ChevronRightIcon color='#00CCBB' />
           </TouchableOpacity>
         </View>
-        <View>
+        <View className='pb-36'>
           <Text className='px-4 pt-6 mb-3 font-bold text-xl'>Menu</Text>
           {/* Dishrows */}
           {dishes.map((dish) => (
